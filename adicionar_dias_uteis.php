9db1c8f61c05e00e23e1eb9ebe29b46ccf5f4fa3
Williams Calado <57382222+williamscalado@users.noreply.github.com>
2020-09-14 23:32:08 -0300

    Update adicionar_dias_uteis.php

--- adicionar_dias_uteis.php
+++ adicionar_dias_uteis.php
@@ -9,49 +9,33 @@ function adicionar_dias_uteis($data_origem,$n_dias){
 	// $n_dias = quantidades de dias a serem adicinados na data
 	// criada 14/09/2020
 	 $feriados = array('01/01', '25/12','01/01');
-
-	 
-	 
+ 
 	 $data_nova = new DateTime($data_origem);		 
-	 $data_nova -> add(new DateInterval('P'.$n_dias.'D'));
-
-
-	// verifico final de semana
-	// 6 = sabado
-	// 0 = domingo
-	$dia_semana = $data_nova->format("w");
-
-	if($dia_semana==6){		
-		$n_sabado = 2;
-		$data_nova -> add(new DateInterval('P'.($n_sabado).'D'));
-	}elseif($dia_semana==0){
-		$n_domingo = 1;
-		$data_nova -> add(new DateInterval('P'.$n_domingo.'D'));
-	}
+	 $data_nova->modify($n_dias.' weekdays'); 
 
 	
-    // verificar se é feriado
-    $verifica_data = $data_nova->format("d/m");
-    if(array_search($verifica_data, $feriados)){
+	// verificar se é feriado
+	$verifica_data = $data_nova->format("d/m");
+	if(array_search($verifica_data, $feriados)){		
+		// é feriado
+		$n_feriado = 1;
+		$data_nova -> modify($n_feriado.' weekdays'); 
+		$data = $data_nova->format("Y-m-d");
+		
+		// faz uma nova verificação 
+		return adicionar_dias_uteis($data,0);
 
-      // é feriado
-      // adicionar mais 1 da a data e verifico
-      $n_feriado = 1;
-      $data_nova -> add(new DateInterval('P'.($n_feriado).'D'));
-      $data = $data_nova->format("Y-m-d");
+	}else{
+		//não é feriado
+		// pode mostar data
+		return  $data_nova->format("Y-m-d");
 
-      // faz uma nova verificação 
-      adicionar_dias_uteis($data,0);
+	}
 
-    }else{
-      //não é feriado
-      // pode mostar data
-      echo  $data_nova->format("Y-m-d");
-
-    }
-
-// fim 
 }
+
+
+
 
 
 adicionar_dias_uteis('2020-09-14',3);
